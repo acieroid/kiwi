75e663a192d1592fb844f5d7397405e32ef4d411
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-12-25 22:06:01 +0100

    Add simple type definitions and example page & wiki

---
+++ Main.hs
@@ -0,0 +1,33 @@
+data AccessMode = ReadWrite | ReadNoWrite | NoReadNoWrite
+                deriving (Show, Eq)
+
+data Page = Page {
+    version :: Int
+  , name :: String
+  , content :: String
+  }
+          deriving (Show, Eq)
+
+data Wiki = Wiki {
+    pages :: [(String, Page)]
+  , access :: AccessMode
+  , password :: Maybe String
+  }
+          deriving (Show, Eq)
+
+examplePage :: Page
+examplePage =
+  Page { version = 1
+       , name = "index"
+       , content = "Hello, *world* !"
+       }
+
+exampleWiki :: Wiki
+exampleWiki =
+  Wiki { pages = [(name examplePage, examplePage)]
+       , access = ReadWrite
+       , password = Nothing
+       }
+
+main :: IO ()
+main = putStrLn (show exampleWiki)
