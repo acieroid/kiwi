8fb691e18e93a0c62d4187e95c209a2525b3a74a
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-01-07 19:48:38 +0100

    Move js and css to wiki/

--- wiki/edit.js
+++ wiki/edit.js
@@ -4,14 +4,9 @@ function error(msg) {
 
 function save(wiki, page) {
     var content = $("#content").val();
-    alert(content);
-    $.post("/api/wiki/" + wiki + "/" + page, content, function(data) {
-        alert(data["result"]);
+    $.post("/wiki/" + wiki + "/" + page, content, function(data) {
+        location.reload()
     });
-
-    $("#action").attr('onclick','').unbind('click');
-    $("#action").html("edit");
-    $("#action").click(function () { edit(wiki, page) });
 }
 
 function edit(wiki, page) {
@@ -21,7 +16,7 @@ function edit(wiki, page) {
 
     $("#main").html("<textarea name=\"content\" id=\"content\">");
 
-    $.get("/api/wiki/" + wiki + "/" + page, function(data) {
+    $.get("/wiki/" + wiki + "/" + page, function(data) {
         $("#content").val(data["content"]);
     }).fail(function() {
         error("cannot access API");
