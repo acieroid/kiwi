debb1053dda5bbaba70df6f804db82e37c4a5080
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-01-08 21:39:26 +0100

    Fix use of cmdargs

--- Kiwi/Config.hs
+++ Kiwi/Config.hs
@@ -3,13 +3,13 @@ module Kiwi.Config where
 
 import Data.Functor ((<$>))
 import System.Environment
-import System.Console.CmdArgs ((&=), cmdArgs, Data, details, explicit, help,
-                               name, program, summary, Typeable)
+import System.Console.CmdArgs ((&=), cmdArgs, Data, details, explicit,
+                               help, name, program, summary, typDir, Typeable)
 
 data Target = ServerAndAPI | Server | API | Generate
             deriving (Show, Data, Typeable)
 
--- TODO: see http://hackage.haskell.org/package/cmdargs-0.1/src/cmdargs.htm
+-- TODO: use modes instead of targets?
 data Args = Args
     { argWikiDir :: FilePath
     , argPort :: Integer
@@ -20,26 +20,32 @@ data Args = Args
     }
             deriving (Show, Data, Typeable)
 
-defaultArgs :: Args
 defaultArgs = Args
-              { argWikiDir = "./wiki" &= explicit &= name "wikidir"
+              { argWikiDir = "./wiki" &= typDir
+                             &= explicit &= name "wikidir"
                              &= help "Wiki directory"
-              , argPort = 8000 &= explicit &= name "port"
+              , argPort = 8000
+                          &= explicit &= name "port"
                           &= help "Listening port"
-              , argHost = "localhost" &= explicit &= name "host"
+              , argHost = "localhost"
+                          &= explicit &= name "host"
                           &= help "Listening host"
-              , argWiki = Nothing &= explicit &= name "wiki"
+              , argWiki = Nothing
+                          &= explicit &= name "wiki"
                           &= help "Wiki to generate"
-              , argPage = Nothing &= explicit &= name "page"
+              , argPage = Nothing
+                          &= explicit &= name "page"
                           &= help "Page to generate"
-              , argTarget = ServerAndAPI &= explicit &= name "target"
+              , argTarget = ServerAndAPI
+                            &= explicit &= name "target"
                             &= help "What to do"
               }
               &= summary "kiwi v0.1"
               &= details ["More information at http://github.com/acieroid/kiwi"]
+              &= program "kiwi"
 
 args :: IO Args
-args = getProgName >>= (\p -> cmdArgs (defaultArgs &= program p))
+args = cmdArgs defaultArgs
 
 wikiDir :: IO String
 wikiDir = argWikiDir <$> args
